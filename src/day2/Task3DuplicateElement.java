5a95a6d29db878b0e5189b46b17af878c484e665
irsadartus <erxat413@gmail.com>
2023-08-02 17:28:50 +0300

    Day2 Task3DuplicateElement.java

---
+++ src/day2/Task3DuplicateElement.java
@@ -0,0 +1,63 @@
+package day2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Task3DuplicateElement {
+
+    /*
+
+    DUbpicate Elements
+
+    Create a method that will take ana array of numbers and return all the elements that are duplicates values.
+
+    The result should only include one occurrence of the duplicates
+
+    Ex:
+
+    input : {1, 3 , 5 , 1 , 9 , 5 , 9 , 6 , 4}
+
+    output : { 1 , 5 , 9}
+
+
+
+     */
+
+
+    public static  int[] finDup (int...arr) {
+
+        int[] dups = new int[arr.length] ; // this could also be an arraylist
+        int index = 0 ;
+        ArrayList<Integer> checked = new ArrayList<>() ;
+
+        for (int each: arr) {
+
+            if (checked.contains(each)) {
+
+                continue;
+            }
+
+            int count = 0 ;
+
+            for (int other : arr) {
+                if (each == other) {
+                    count++ ;
+                }
+
+            }
+
+            if (count > 1) {
+                dups[index++] = each ;
+                checked.add(each) ;
+            }
+
+        }
+
+        return  Arrays.copyOf(dups , checked.size()) ;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(Arrays.toString(finDup(1, 3 , 5 , 1 , 9 , 5 , 9 , 6 , 4)));
+    }
+}
